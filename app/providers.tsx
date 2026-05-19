7be11ec6fee9f93428fb6589dd3fcce8dadc0bb7
Devansh Soni <devansh.nitb@yahoo.com>
2026-05-20 02:18:41 +0530

    Fix/react query devtools prod [GSSoC 2026] (#604)
    * Updated Syntax Issue
    
    * Patch to Prevent ReactDevtools in Prod. Builds

--- app/providers.tsx
+++ app/providers.tsx
@@ -41,7 +41,9 @@ export function Providers({ children }: { children: ReactNode }) {
           </TooltipProvider>
         </ThemeProvider>
       </AuthProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
+      {process.env.NODE_ENV === 'development' && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
     </QueryClientProvider>
   );
 }
